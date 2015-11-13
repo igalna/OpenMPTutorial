cfb3709a39bea6b80c291505c21cd901a721d81b
igalna <igalna@gmail.com>
2015-11-13 16:36:13 +0000

    Hello World

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include <stdio.h>
 #include <omp.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hey you I'm alive!" << endl;
-    return 0;
+
+    int ID = 0;
+
+    printf(" hello(%d)", ID);
+    printf(" world(%d) \n", ID);
 }
